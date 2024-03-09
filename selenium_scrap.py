b4738f3b540fb22246abc749e1ad473e2e649bae
hgfejcwoefGleb <gipilipenko@edu.hse.ru>
2024-03-09 22:21:49 +0300

    Second step

--- selenium_scrap.py
+++ selenium_scrap.py
@@ -24,28 +24,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def respond():
     if request.method == "GET":
-
-        data = request.json
-        command = data.get('request', {}).get('command', '')
-
-        end_session = False
-
-        if 'выход' in command:
-            response_text = 'До свидания!'
-            end_session = True
-        elif command:
-            response_text = f'Вы сказали {command, download_selenium()}'
-        else:
-            response_text = 'Привет! Вы ничего не сказали.'
-
-        response = {
-            'response': {
-                'text': response_text,
-                'end_session ': end_session
-            },
-            'version': '1.0'
-        }
-        return response
+        return download_selenium()
 
 
 app.run(host='0.0.0.0', port=5000, debug=True)
