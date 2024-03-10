0fa9c931d259b9ecd251514918b0f0ea202ef343
hgfejcwoefGleb <gipilipenko@edu.hse.ru>
2024-03-10 18:15:49 +0300

    Second step

--- selenium_scrap.py
+++ selenium_scrap.py
@@ -1,3 +1,4 @@
+import time
 
 from selenium import webdriver
 from flask import Flask, request
@@ -15,6 +16,7 @@ def download_selenium():
     service = webdriver.ChromeService(executable_path=".\\drivers\\chromedriver.exe")
     driver = webdriver.Chrome(service=service, options=chrome_options)
     auth_url = "https://auth.hse.ru/adfs/oauth2/authorize/?client_id=35db5abd-b262-4923-9d0e-00c56140be41&redirect_uri=https://lk.hse.ru/api/adfs-auth&response_mode=form_post&response_type=code"
+    time.sleep(5)
     driver.get(auth_url)
     title = driver.title
     return title
