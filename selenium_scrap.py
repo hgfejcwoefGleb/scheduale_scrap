505a7ac30eca9dc6aa05f5c88cc7c73b8dcdcdea
hgfejcwoefGleb <gipilipenko@edu.hse.ru>
2024-03-10 18:03:39 +0300

    Second step

--- selenium_scrap.py
+++ selenium_scrap.py
@@ -12,7 +12,8 @@ def download_selenium():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    service = webdriver.ChromeService(executable_path=".\\drivers\\chromedriver.exe")
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     auth_url = "https://auth.hse.ru/adfs/oauth2/authorize/?client_id=35db5abd-b262-4923-9d0e-00c56140be41&redirect_uri=https://lk.hse.ru/api/adfs-auth&response_mode=form_post&response_type=code"
     driver.get(auth_url)
     title = driver.title
